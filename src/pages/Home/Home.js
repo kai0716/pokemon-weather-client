f5ae3116bdf489f0928d004e59d5888a301d3a1e
revyrob <kayle.robson@gmail.com>
2022-09-01 11:10:12 -0700

    working on weather api

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,49 +1,64 @@
-import { Link, useNavigate } from 'react-router-dom'
-import axios from 'axios';
-import './Home.scss'
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./Home.scss";
+// import { useState, useEffect } from "react";
 
 export default function UploadPage() {
+  //useState for weather in main bar
+  //   const [weather, setWeather] = useState(null);
 
-    let nav = useNavigate();
-    const Navigate = (event) => {
-        event.preventDefault();
-        console.log(event.target.title.value);
+  //variables for key and url, can store this in another place in the future
+  const urlWeather = `https://api.weatherbit.io/v2.0/current?lat=49.2850&lon=-123.1147`;
+  const apiKey = `&key=e840589dcb26493795dcd26a7469f83b&include=minutely`;
 
-        axios.post(`http://localhost:8080/videos`, {
-            title: event.target.title.value,
-            description: event.target.description.value
-        })
-            .then(response => {
-                console.log(response.data);
-                alert("Upload success");
-                setTimeout(() => {
-                    nav('/');
-                }, 1000);
-            })
-            .catch((err) => console.log(err));
+  //get the list for the videos
+  //useEffect(() => {
+  axios
+    .get(`${urlWeather}${apiKey}`)
+    .then((response) => {
+      const weather = response.data.data[0].app_temp;
+    })
+    .catch((err) => console.log(err));
+  //}, []);
 
-    }
+  let nav = useNavigate();
+  const Navigate = (event) => {
+    event.preventDefault();
+    console.log(event.target.title.value);
 
-    return (
-        <div className='pokemon'>
-            <div className='pokemon__weather'>
-                <div className='pokemon__weather-wrapper'>
-                    <p className='pokemon__weather-info'>Data</p>
-                </div>
-            </div>
-            <p className='content'>It is ______(fill in with  warm, hot, or cold).  A great mocktail
-                to share today with your pokemon is....
-            </p>
-            <div className='pokemin__mocktail'>
-                <div className='pokemin__mocktail-wrapper'>
-                    <p>Data</p>
-                </div>
-            </div>
+    axios
+      .post(`http://localhost:8080/videos`, {
+        title: event.target.title.value,
+        description: event.target.description.value,
+      })
+      .then((response) => {
+        console.log(response.data);
+        alert("Upload success");
+        setTimeout(() => {
+          nav("/");
+        }, 1000);
+      })
+      .catch((err) => console.log(err));
+  };
 
-            <div className='pokemin__image'>
-
-            </div>
-
+  return (
+    <div className="pokemon">
+      <div className="pokemon__weather">
+        <div className="pokemon__weather-wrapper">
+          <p className="pokemon__weather-info">{weather}</p>
         </div>
-    )
+      </div>
+      <p className="content">
+        It is ______(fill in with warm, hot, or cold). A great mocktail to share
+        today with your pokemon is....
+      </p>
+      <div className="pokemin__mocktail">
+        <div className="pokemin__mocktail-wrapper">
+          <p>Data</p>
+        </div>
+      </div>
+
+      <div className="pokemin__image"></div>
+    </div>
+  );
 }
