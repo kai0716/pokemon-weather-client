819da67ae0cc128ba82164535d980dd39a29314e
kai0716 <kevin-zhang98@hotmail.com>
2022-09-01 14:10:55 -0700

    display pokemon

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./Home.scss";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function UploadPage() {
     //useState for weather in main bar
@@ -21,6 +21,60 @@ export default function UploadPage() {
             .catch((err) => console.log(err));
     }, []);
 
+
+    //--------------------------Kevin Part------------------------------------
+
+    const [pokemonData, setPokemonData] = useState(null);
+
+    const [pokemonDetail, setPokemonDetail] = useState(null);
+    const [list, setList] = useState([]);
+    const weather = "sunny";
+
+    const findPokemonWithName = (pokemonName) => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(response => {
+
+            const pokemon = {
+                name: pokemonName,
+                url: response.data.sprites.front_default
+            }
+
+            setList((prev) => [...prev, pokemon])
+
+        }).catch(error => {
+            console.error(`Couldn't get a response from the API: ${error}`);
+        });
+    }
+
+    const getType = (type) => {
+        axios.get("https://pokeapi.co/api/v2/type/")
+            .then((response) => {
+
+                let type = response.data.results.find((e) => e.name === type);
+                console.log(type.url);
+                axios.get(`${type.url}`)
+                    .then((response) => {
+                        console.log(response.data);
+                    });
+
+            });
+    }
+
+    useEffect(() => {
+        axios.get('https://pokeapi.co/api/v2/pokemon/?limit=200').then(response => {
+            setPokemonData(response.data.results);
+
+            response.data.results.forEach(element => {
+                findPokemonWithName(element.name)
+
+            });
+
+
+        }).catch(err => {
+            console.error(err);
+        });
+    }, []);
+
+
     return (
         <div className='pokemon'>
             <div className='pokemon__weather'>
@@ -37,15 +91,15 @@ export default function UploadPage() {
                 </div>
             </div>
 
-            <div className='pokemin__image'>
-                <div class="marquee">
-                    <div class="marquee-content">
-                        <div class="marquee-item">
-                            <img src="https://via.placeholder.com/600/000000/FFFFFF/?text=01" alt="" />
-                        </div>
-                        <div class="marquee-item">
-                            <img src="https://via.placeholder.com/600/000000/FFFFFF/?text=01" alt="" />
-                        </div>
+            <div className='pokemon__image'>
+                <div className="marquee">
+                    <div className="marquee-content">
+                        {list && shuffleArray(list).map((e, i) => {
+                            return (
+                                <div key={i} className="marquee-item">
+                                    <img src={e.url} alt="pokemon image" />
+                                </div>)
+                        })}
 
                     </div>
                 </div>
@@ -55,3 +109,12 @@ export default function UploadPage() {
 
 }
 
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
